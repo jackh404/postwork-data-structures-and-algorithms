569e5edbaf1178db8123a0150fe0833af2200418
Jack Henza <jackhenza@gmail.com>
2024-02-16 15:56:04 -0500

    complete thru w2d2

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,5 +1,7 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num > 9) return;
+  console.log(num);
+  recursiveCount(num + 1);
 }
 
 if (require.main === module) {
--- 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
+++ 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
@@ -1,5 +1,11 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  if (!arr.length) {
+    return false;
+  }
+  if (arr[0] === target) {
+    return true;
+  }
+  return recursiveSearch(arr.slice(1), target);
 }
 
 if (require.main === module) {
@@ -11,9 +17,17 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", recursiveSearch([3, 2, 1], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", recursiveSearch([15, 2, 3, 9, 7, 8, 3, 6, 4, 4], 3));
 }
 
 module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// pop a value and check if it matches, if so return true
+// if arr.length is 0, return false
+// call recursiveSearch with altered array
