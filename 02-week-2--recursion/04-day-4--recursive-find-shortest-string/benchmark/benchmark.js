6d2a176e5da7509324a72b4ebf2f1401626f7e0c
Jack Henza <jackhenza@gmail.com>
2024-03-05 17:33:37 -0500

    complete w2d4

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/benchmark/benchmark.js
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/benchmark/benchmark.js
@@ -1,17 +1,34 @@
 // Paste your iterative and recursive solutions in this file
 // And then calculate their average run times to compare them.
+function findShortestStringRecursive(arr) {
+  if(arr.length < 2) return arr[0]
+  if(arr[0].length > arr[1].length){
+    return findShortestStringRecursive(arr.slice(1))
+  } else{ 
+    arr.splice(1,1)
+    return findShortestStringRecursive(arr)
+  }
+}
+
+function findShortestString(arr) {
+  let ret = arr[0];
+  for (n = 1; n < arr.length; n++) {
+    if (arr[n].length < ret.length) ret = arr[n];
+  }
+  return ret;
+}
 
 
-
-
-
+const arr = [ 'if', 'juniper', 'lily', 'dandelion', 'do','shooby','I','am','a','jelly','donut','fdjkasjflajsd' ]
 
 function benchmark(callback) {
   const startTime = Date.now();
 
   for (let i = 0; i < 1000; ++i) {
-    callback();
+    callback(arr);
   }
 
   return (Date.now() - startTime) / 1000;
 }
+console.log(benchmark(findShortestString))
+console.log(benchmark(findShortestStringRecursive))
--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
@@ -1,7 +1,12 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  if(arr.length < 2) return arr[0]
+  if(arr[0].length > arr[1].length){
+    return findShortestStringRecursive(arr.slice(1))
+  } else{ 
+    arr.splice(1,1)
+    return findShortestStringRecursive(arr)
+  }
 }
-
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
@@ -16,9 +21,21 @@ if (require.main === module) {
 
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
+
+  console.log("Expecting: 'if'");
+  console.log("=>", findShortestStringRecursive(['flower', 'if', 'juniper', 'lily', 'dandelion', 'do']));
 }
 
 module.exports = findShortestStringRecursive;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/*
+Recursively find the (first) shortest string in an array.
+
+if the array only has one string, return it
+else
+compare the first two strings of the array
+call the function with the longer of the two strings (or the second one if they are of equal length) removed
+*/
