3e7139d4e4a3742d9e48070548ea96d394a9079d
Jack Henza <jackhenza@gmail.com>
2024-03-27 18:06:18 -0400

    complete w3b4

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
@@ -1,5 +1,13 @@
 function distinctPairSum(arr, k) {
-  // type your code here
+  const outputArray = [];
+  arr.forEach((e, i) => {
+    if (e + arr[i + 1] === k) {
+      if (!outputArray.find(sub => sub.indexOf(e) >= 0)) {
+        outputArray.push([e, arr[i + 1]]);
+      }
+    }
+  });
+  return outputArray;
 }
 
 if (require.main === module) {
@@ -11,9 +19,32 @@ if (require.main === module) {
 
   console.log("Expecting: [[2, 8]]");
   console.log("=>", distinctPairSum([3, 4, 2, 1, 5, 2, 8, 2], 10));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", distinctPairSum([], 1));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", distinctPairSum([2, 8, 6, 1, 4, 7, 5], 2));
 }
 
 module.exports = distinctPairSum;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/* Return an array of arrays. The inner arrays
+ * should be pairs of consecutive numbers that add
+ * up to a given value. The inner arrays should not
+ * be repeated, including in a different order.
+ * ie, [2,8] for 10 is the same as [8,2]
+ *
+ * create blank outputArray
+ * Loop through array
+ *  check if array[n] + array[n+1] === k
+ *    if so, check if array[n] is in any array in outputArray
+ *      if not, add [array[n],array[n+1]] to outputArray
+ * return outputArray
+ *  */
