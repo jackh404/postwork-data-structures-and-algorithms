b1051d87f4539cb2d716f6612d5e253b7dfca529
Jack Henza <jackhenza@gmail.com>
2024-04-01 16:48:45 -0400

    complete w3b5

--- 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
+++ 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
@@ -1,19 +1,37 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const arr = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let k = i + 1; k < string.length + 1; k++) {
+      arr.push(string.slice(i, k));
+    }
+  }
+  return arr;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(""));
 }
 
 module.exports = consecutiveSubstrings;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/* Given an input string, output all possible substrings consisting of
+ * consecutive characters.
+
+ * Loop through string (pos)
+    * loop through all letters after current letter (pos2)
+    *    output string from pos to pos2*/
